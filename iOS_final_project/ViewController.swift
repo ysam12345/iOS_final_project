6ff184bb962b4fa94d229c5e017b0a7a91be91e7
游子謙 <ysam12345@gmail.com>
2019-01-03 20:56:12 +0800

    Initial Commit

---
+++ iOS_final_project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_final_project
+//
+//  Created by yochien on 2019/1/3.
+//  Copyright © 2019年 yochien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
